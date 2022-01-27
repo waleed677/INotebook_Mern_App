6d5b80c09b21559aee49f88a20ada0f86521c498
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-01-27 23:34:12 +0500

    add , get notes from mongodb

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,7 +1,12 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
    title: {
          type: String,
             required: true
@@ -20,4 +25,4 @@ const NotesSchema = new Schema({
 
   });
 
-  module.exports = mongoose.model('Notes', UserSchema);
+  module.exports = mongoose.model('Notes', NotesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchUser = require('../middlewares/fetchUser');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
- obj ={
-    a: "Demo",
-    number : 30
- }
-    res.json(obj);
+  // Route 1 Add NOtes to DB Using the POST 
+router.post('/createNotes',fetchUser,[
+   body('title','Please enter a valid Title').isLength({min:3}),
+   body('description', 'Please enter a valid description').isLength({min:5}),
+   body('tag', 'Please enter a valid tag').isLength({min:5}),
+], async (req, res) => {
+   try {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+         return res.status(400).json({errors: errors.array()});
+      }
+      const {title , description , tag} = req.body;
+   
+      const notes = new Notes({
+         title,description,tag,user:req.user.id
+      })
+      const savedNotes = notes.save();
+      res.json(savedNotes);
+   } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+   }
+  
 });
+
+
+  // Route 2 Get All Notes from DB Using the GET 
+
+  router.get('/getAllNotes', fetchUser , async(req,res) => {
+     try {
+      const notes = await Notes.find({user:req.user.id})
+      res.json(notes);
+     } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+     }
+  
+  });
+
+ 
+
+
 
  module.exports = router;
